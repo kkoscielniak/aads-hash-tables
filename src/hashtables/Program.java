0226f05062508155493068679d3647ce33cca5a5
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2016-05-31 20:25:48 +0200

    xorshf96() method

---
+++ src/hashtables/Program.java
@@ -0,0 +1,23 @@
+package hashtables;
+
+/**
+ * Entry level of the program
+ * @author krystian
+ */
+public class Program {
+    
+    /**
+     * Determines type of hashing
+     */
+    public enum HashingMethod { 
+        LP, 
+        DH 
+    }
+
+    /**
+     * @param args the command line arguments
+    */
+    public static void main(String[] args) {       
+        System.out.println(Utils.getInstance().xorshf96());
+    }   
+}
---
+++ src/hashtables/Utils.java
@@ -0,0 +1,41 @@
+package hashtables;
+
+/**
+ * Provides some simple utilities
+ * @author krystian
+ */
+public class Utils {
+    
+    private static Utils instance = null;
+    
+    private static Long x = (long) 123456789;
+    private static Long y = (long) 362436069;
+    private static Long z = (long) 521288629;
+    
+    public static Utils getInstance() {
+        
+        if (instance == null) {
+            instance = new Utils();
+        }        
+        return instance;
+    }
+    
+    /**
+     * Generates big number, algorithm given by the tutor :)
+     * @return
+     */
+    public static Long xorshf96() {
+        
+        Long tmp; 
+        
+        x ^= x << 16; 
+        x ^= x >> 5;
+        x ^= x << 1;
+        
+        tmp = x;
+        x = y; 
+        y = z; 
+        z = tmp^x^y;
+        return z;         
+    }
+}
